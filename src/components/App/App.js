d7e6be0658cee3be03912605ab458934fc75edcf
Daniel Calvanese <Daniel.Calvanese@Gmail.com>
2020-11-27 14:01:53 -0500

    Added module 3 tasks.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
-import { createTiles } from '../../misc/utils'
+import { createTiles, indexOfSelected } from '../../misc/utils'
 
 import './App.css';
 
@@ -23,8 +23,43 @@ class App extends Component {
       playing: true,
       previousTileIndex: null,
       toBeCleared: null,
-      tiles: createTiles(state.numTiles),
+      tiles: createTiles(state.numTiles, this.handleTileClicked),
     }));
+  }
+
+  handleTileClicked = (id, color) => {
+    this.setState((state) => {
+      const tiles = state.tiles
+      let toBeCleared = state.toBeCleared
+      let previousTileIndex = state.previousTileIndex
+      const selectedTileIndex = indexOfSelected(tiles, id, color)
+
+      if (toBeCleared !== null) {
+        tiles[toBeCleared[0]].selected = false
+        tiles[toBeCleared[1]].selected = false
+        toBeCleared = null
+      }
+
+      tiles[selectedTileIndex].selected = true
+
+      if (previousTileIndex !== null) {
+        const previousTile = tiles[previousTileIndex]
+        const selectedTile = tiles[selectedTileIndex]
+
+        if (previousTile.id !== selectedTile.id && previousTile.color === color) {
+          selectedTile.matched = true
+          previousTile.matched = true
+          previousTileIndex = null
+        } else {
+          toBeCleared = [previousTileIndex, selectedTileIndex]
+          previousTileIndex = null
+        }
+      } else {
+        previousTileIndex = selectedTileIndex
+      }
+
+      return { toBeCleared, tiles, previousTileIndex }
+    })
   }
 
   render() {
